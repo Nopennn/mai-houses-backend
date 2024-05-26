24607efa6c6ba58bf71028cfa34164c32c544d89
Gleb_Makarov <74141032+YourMakarov@users.noreply.github.com>
2024-05-26 15:26:18 +0300

    all user filds are required for signup

--- validators/upd_user_validator.js
+++ validators/upd_user_validator.js
@@ -2,18 +2,18 @@ let Validator = require('validatorjs');
 
 const fun = (body) => {
     const rule = {
-        login: 'required|string|min:2|max:25',
-        password: 'required|string|min:2|max:255',
-        email: 'required|email',
-        age: 'required|integer|min:0',
-        name: 'required|string|min:2|max:255',
-        surname: 'required|string|min:2|max:255',
-        photo: 'required|string',
-        gender: ["required",{ 'in': ["none", "m" , "f"] }],
-        about: 'required|string',
-        wanted: 'required|string',
-        tags: 'required|array',
-        
+      login: 'required|string|min:2|max:25',
+      password: 'required|string|min:2|max:255',
+      name: 'required|string',
+      surname: 'required|string',
+      photo: 'required|string',
+      email: 'required|string|email',
+      phone: 'required|string',
+      age: 'required|integer',
+      gender: ["required", { 'in': ["none", "m" , "f"] }],
+      about: 'required|string',
+      wanted: 'required|string',
+      tags: 'required|array',
       };
     return new Validator(body, rule).fails()
 }
